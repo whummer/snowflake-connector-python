20228b83133816267831c3d1f18157a1e3774b29
Waldemar Hummer <waldemar.hummer@gmail.com>
2024-05-30 04:16:03 +0200

    add parameters file; more cleanups

---
+++ test/parameters.py
@@ -0,0 +1,12 @@
+#
+# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+#
+
+CONNECTION_PARAMETERS = {
+    "account": "test",
+    "user": "test",
+    "password": "test",
+    "database": "test",
+    "schema": "public",
+    "host": "snowflake.localhost.localstack.cloud",
+}
